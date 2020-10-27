8cf33be0131ca9e8364e1d9ffe638618faacb963
Joseph Lavagna <josephlavagna1998@gmail.com>
2020-10-27 11:26:33 +0000

    implemented model

--- API/Models/DiseaseModel.cs
+++ API/Models/DiseaseModel.cs
@@ -1,9 +1,16 @@
+using API.Models;
 using System;
+using System.Collections.Generic;
 
 namespace API
 {
     public class DiseaseModel
     {
+        public string DiseaseTerm { get; set; }
+        public string Biobank { get; set; }
+        public List<string> AssociatedData { get; set; } 
+        public List<string> SampleTypes { get; set; }
+        public List<DonorModel> Donors { get; set; }
 
     }
 }
