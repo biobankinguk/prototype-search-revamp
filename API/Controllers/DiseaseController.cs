011fa666c300ca92a4ec231840460e28450df449
Joseph Lavagna <josephlavagna1998@gmail.com>
2020-10-27 10:43:56 +0000

    renamed and removed api dummy code

---
+++ API/Controllers/DiseaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api[controller]")]
+    public class DiseaseController : ControllerBase
+    {
+        private readonly ILogger<DiseaseController> _logger;
+        public DiseaseController(ILogger<DiseaseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<DiseaseModel> Get()
+        {
+
+        }
+    }
+}
