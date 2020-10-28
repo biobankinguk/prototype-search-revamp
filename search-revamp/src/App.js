11625bccacdb0c064634316de81eceee77f26f3c
Joseph Lavagna <josephlavagna1998@gmail.com>
2020-10-28 16:15:04 +0000

    basic chakra ui header added

--- search-revamp/src/App.js
+++ search-revamp/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import logo from './images/logo.svg';
+import { ThemeProvider, CSSReset, Heading } from "@chakra-ui/core"
 import './App.css';
+import Header from "./header";
 
 function App() {
   return (
+    <ThemeProvider>
+      <CSSReset />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
+    </ThemeProvider>
   );
 }
 
