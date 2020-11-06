12342b3206de8c7f149865e011ad9c88e5b84ffa
Joseph Lavagna <josephlavagna1998@gmail.com>
2020-11-06 13:33:16 +0000

    implemented getSamples function

--- search-revamp/src/components/samples.js
+++ search-revamp/src/components/samples.js
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
+import { useDisease } from "./API/diseaseApi";
 import { SimpleGrid, Flex, Heading, Text, Box, Stack } from "@chakra-ui/core"
 
-function Feature({ title, desc, ...rest }) {
-    return (
-    <Box p={5} shadow="md" borderWidth="1px" {...rest}>
-        <Heading fontSize="xl">{title}</Heading>
-        <Text mt={4}>{desc}</Text>
-    </Box>
-    );
+const GetSamples = () => {
+  const res = useDisease()
+  const diseaseTerm = ""
+  const biobank = ""
+  const sampleTypeList = []
+  const data = {res, diseaseTerm, biobank, sampleTypeList}
+  if (res.isLoading){
+    return null
   }
+  else {
+    {res.diseases.map(item => {
+      item.diseaseTerm = diseaseTerm
+      item.biobank = biobank
+      for (const sample of item.sampleTypes){
+        sampleTypeList.push(
+            <div>{sample}</div>
+        )
+      }
+
+      })
+  }
+  return data
+  }
+}
+
 
 function Samples() {
     return (
       <Flex padding="30px">
-                <Box bg="grey" width="1000px" height="300px" >
+                <Box bg="#EBEBEB" width="1000px" height="300px" >
                          <Stack spacing={3}>
                             <Text fontSize="3xl">Sample Details</Text>
                             <Text fontSize="xl">Sample Type</Text>
